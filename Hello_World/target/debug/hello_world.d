be19a5dc808b4d8ae6c0631c86296e2eeac77a6f
Gurpreet Singh <gsingh@localhost.localdomain>
2016-06-06 14:10:51 -0500

    cleanup

---
+++ Hello_World/target/debug/hello_world.d
@@ -0,0 +1,5 @@
+/home/gsingh/Documents/Projects/LearnRust/Hello_World/target/debug/hello_world.d: src/main.rs
+
+/home/gsingh/Documents/Projects/LearnRust/Hello_World/target/debug/hello_world: src/main.rs
+
+src/main.rs:
