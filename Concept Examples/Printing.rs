7b4423f7270ec124be6088a1ddcb9fc6c6264b47
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-14 16:17:20 -0400

    learned how to print structs

--- Concept Examples/Printing.rs
+++ Concept Examples/Printing.rs
@@ -1,3 +1,13 @@
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+// is a structure which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() 
 {
     // In general, the `{}` will be automatically replaced with any
@@ -33,10 +43,24 @@ fn main()
     // FIXME ^ Add the missing argument: "James"
 
     // Create a structure which contains an `i32`. Name it `Structure`.
-    struct Structure(i32);
+    // struct Structure(i32);
 
     // However, custom types such as this structure require more complicated
     // handling. This will not work.
     // println!("This struct `{}` won't print...", Structure(3));
     // FIXME ^ Comment out this line.
+
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
 }
