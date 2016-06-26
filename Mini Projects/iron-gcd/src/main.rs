c61e9a9504be5bd1ea30d546e5fcb8fe1348c6fe
root <Guppst3r@gmail.com>
2016-06-26 16:54:12 -0400

    Learning about Iron

--- Mini Projects/iron-gcd/src/main.rs
+++ Mini Projects/iron-gcd/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+extern crate iron;
+#[macro_use] extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main()
+{
+        println!("Serving on http://localhost:3000...");
+        Iron::new(get_form).http("localhost:3000").unwrap();
 }
+
+#[allow(unused_variables)]
+fn get_form(request: &mut Request) -> IronResult 14 fn get_form(request: &mut Request) -> IronResult <Response>
+{
+
+}
+
