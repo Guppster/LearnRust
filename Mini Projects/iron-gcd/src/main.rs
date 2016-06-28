31ea9851092556e4a90d36ec2f8c9e1c6f912216
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-28 15:58:03 -0400

    Added GCD function. Added tests. Fixed typos. Webpage works now!

--- Mini Projects/iron-gcd/src/main.rs
+++ Mini Projects/iron-gcd/src/main.rs
@@ -16,36 +16,36 @@ fn main()
 	router.get("/", get_form);
 	router.post("/gcd", post_gcd);
 
-        println!("Serving on http://localhost:3000...");
-        Iron::new(router).http("localhost:3000").unwrap();
+	println!("Serving on http://localhost:3000...");
+	Iron::new(router).http("localhost:3000").unwrap();
 }
 
 #[allow(unused_variables)]
 fn get_form(request: &mut Request) -> IronResult <Response>
 {
-        let mut response = Response::new();
+	let mut response = Response::new();
 
-        response.set_mut(status::Ok);
-        response.set_mut(mime!(Text/Html; Charset=Utf8));
-        response.set_mut(r#"
-                        <title>GCD Calculator</title>
-                        <form action="/gcd" method="post">
-                        <input type="text" name="n"/>
-                        <input type="text" name="n"/>
-                        <button type="submit">Compute GCD</button>
-                        </form>
-                        "#);
+	response.set_mut(status::Ok);
+	response.set_mut(mime!(Text/Html; Charset=Utf8));
+	response.set_mut(r#"
+			<title>GCD Calculator</title>
+			<form action="/gcd" method="post">
+			<input type="text" name="n"/>
+			<input type="text" name="n"/>
+			<button type="submit">Compute GCD</button>
+			</form>
+			"#);
 
-        Ok(response)
+	Ok(response)
 }
 
 fn post_gcd(request: &mut Request) -> IronResult<Response>
 {
 	let mut response = Response::new();
-	
+
 	let hashmap;
-	
-	match request.get_ref::<URLEncodedBody>
+
+	match request.get_ref::<UrlEncodedBody>()
 	{
 		Err(e) => 
 		{
@@ -53,10 +53,10 @@ fn post_gcd(request: &mut Request) -> IronResult<Response>
 			response.set_mut(format!("Error parsing form data: {:?}\n", e));
 			return Ok(response);
 		}
-		
+
 		Ok(map) => { hashmap = map; }
 	}
-	
+
 	let unparsed_numbers;
 
 	match hashmap.get("n")
@@ -67,10 +67,10 @@ fn post_gcd(request: &mut Request) -> IronResult<Response>
 			response.set_mut(format!("form data has no 'n' parameter\n"));
 			return Ok(response);
 		}
-		
+
 		Some(nums) => { unparsed_numbers = nums; }
 	}
-	
+
 	let mut numbers = Vec::new();
 
 	for number in unparsed_numbers
@@ -80,10 +80,10 @@ fn post_gcd(request: &mut Request) -> IronResult<Response>
 			Err(_) => 
 			{
 				response.set_mut(status::BadRequest);
-				response.set_mut(format!("Value for 'n' parameter not a number: {:?}\n", number);
+				response.set_mut(format!("Value for 'n' parameter not a number: {:?}\n", number));
 				return Ok(response);
 			}
-			
+
 			Ok(n) => { numbers.push(n); }
 		}
 	}
@@ -96,5 +96,30 @@ fn post_gcd(request: &mut Request) -> IronResult<Response>
 
 	response.set_mut(status::Ok);
 	response.set_mut(mime!(Text/Html; Charset=Utf8));
-	response.set_mut(format!("The greatest common divisor of the numbers {:?} is <b>{}</b>\n, numbers, d))
+	response.set_mut(format!("The greatest common divisor of the numbers {:?} is <b>{}</b>\n", numbers, d));
+
+	Ok(response)
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+	assert!(n != 0 && m != 0);
+	while m != 0 {
+		if m < n {
+			let t = m; m = n; n = t;
+		}
+		m = m % n;
+	}
+	n
+}
+
+#[test]
+fn test_gcd() {
+	assert_eq!(gcd(2 * 5 * 11 * 17,
+				3 * 7 * 13 * 19),
+			1);
+
+	assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+				3 * 7 * 11 * 13 * 19),
+			3 * 11);
+}
+
