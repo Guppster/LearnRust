1e07a481433a04bf7537453902ee3572996bd15a
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-28 16:22:30 -0400

    Learning about complex numbers in Rust (needed for Mandelbrot?)

---
+++ Concept Examples/complexNumbers.rs
@@ -0,0 +1,13 @@
+extern create num;
+use num::Complex;
+
+#[allow](dead_code)]
+fn complex_square_add_loop(c: Complex<f64>)
+{
+	let mut z = Complex { re: 0.0, im: 0.0 };
+	
+	loop
+	{
+		z = z * z + c;
+	}
+}
