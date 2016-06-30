9a163913f2a7d9d5a7ed0a743e282c649db494a5
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-30 13:38:55 -0400

    fixed typos

--- Mini Projects/MandelbrotPlot/src/main.rs
+++ Mini Projects/MandelbrotPlot/src/main.rs
@@ -19,11 +19,11 @@ fn parse_pair<T: FromStr>(s: &str, separator: car) -> Option<(T, T)>
 #[test}
 fn test_parse_pair()
 {
-	assert_eq!{parse_pair::<i32>("", ',', None);
-		assert_eq!(parse_pair::<i32>("10,",     ','), None);
-		assert_eq!(parse_pair::<i32>(",10",     ','), None);
-		assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
-		assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
-		assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
-		assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
-	}
+	assert_eq!(parse_pair::<i32>("",        ','), None);
+	assert_eq!(parse_pair::<i32>("10,",     ','), None);
+	assert_eq!(parse_pair::<i32>(",10",     ','), None);
+	assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+	assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+	assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+	assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
