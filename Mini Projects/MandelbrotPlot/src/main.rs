8305a8707c381d88afff61d4d54f76d9b53ac191
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-30 15:14:30 -0400

    Added rendering function

--- Mini Projects/MandelbrotPlot/src/main.rs
+++ Mini Projects/MandelbrotPlot/src/main.rs
@@ -10,20 +10,20 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)>
 	{
 		//if it doesn't return none
 		None => None,
-		
-		//If it does exist, capture the index from the return of find()
-		Some(index) => 
-		{
-			//This statement splits the string at the seperator and parses both sides from string to T
-			match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) 
-			{
-				//if the above match expression is successful return the option pair
-				(Ok(l), Ok(r)) => Some((l,r)),
-				
-				//if it is anything else return None.	
-				_ => None
-			}
-		}
+
+		     //If it does exist, capture the index from the return of find()
+		     Some(index) => 
+		     {
+			     //This statement splits the string at the seperator and parses both sides from string to T
+			     match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) 
+			     {
+				     //if the above match expression is successful return the option pair
+				     (Ok(l), Ok(r)) => Some((l,r)),
+
+				     //if it is anything else return None.	
+				     _ => None
+			     }
+		     }
 	}
 }
 
@@ -42,13 +42,13 @@ fn test_parse_pair()
 //Return the point on the complex plane corresponding to a given pixel in the bitmap
 //Takes in 4 tuples as arguements and returns a tuple of f64s
 fn pixel_to_point(bounds: (usize, usize),
-		  pixel: (usize, usize),
-		  upper_left: (f64, f64),
-		  lower_right: (f64, f64)) -> (f64,f64)
+		pixel: (usize, usize),
+		upper_left: (f64, f64),
+		lower_right: (f64, f64)) -> (f64,f64)
 {
 	//The tuple.0 or tuple.1 syntax is used to access elements in the tuple
 	let (width, height) = (lower_right.0 - upper_left.0, upper_left.1 - lower_right.1);
-	
+
 	//Since this is the last statement in the function, it is the return
 	(upper_left.0 + pixel.0 as f64 * width / bounds.0 as f64, upper_left.1 - pixel.1 as f64 * height / bounds.1 as f64)
 }
@@ -57,4 +57,29 @@ fn pixel_to_point(bounds: (usize, usize),
 fn test_pixel_to_point()
 {
 	assert_eq!(pixel_to_point((100,100), (25, 75)m (-1.0, 1.0), (1.0, -1.0)), (-0.5, -0.5));
+}
+
+//Render a rectangle of the Mandelbrot set into a buffer of pixels
+//The bounds argument gives the width and the height of the buffer 'pixels'
+//Which hold one grayscale pixel per byte. The upper_left and lower_right
+//arguments specify points on the complex plane corresponding to the upper left
+//and lower right corners of the pixel buffer
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: (f64, f64), lower_right: (f64,f64))
+{
+	assert!(pixels.len() == bounds.0 * bounds.1);
+
+	for r in 0 .. bounds.1
+	{
+		for c in 0 .. bounds.0
+		{
+			let point = pixel_to_point(bounds, (c, r), upper_left, lower_right);
+
+			pixels[r * bounds.0 + c] = 
+				match escapes(Complex {re: point.0, im: point.1 }, 255)
+				{
+					None => 0,
+					Some(count) => 255 - count as u8
+				}
+		}
+	}
 }
