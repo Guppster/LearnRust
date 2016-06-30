4b8493921febb11ee46c8acda8b9ab0b0ea273de
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-30 13:54:10 -0400

    added comments because it seemed confusing

--- Mini Projects/MandelbrotPlot/src/main.rs
+++ Mini Projects/MandelbrotPlot/src/main.rs
@@ -1,18 +1,29 @@
 use std::str:FromStr;
 
-fn parse_pair<T: FromStr>(s: &str, separator: car) -> Option<(T, T)> 
+//<T: FromStr> is "For any type T that implements the FromStr trait"
+//This allows us to use <u32>, <f64>, etc
+//This method requires a string and a seperator and returns a option pair of type T 
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> 
 {
+	//First check to see that the seperator exists in the string
 	match s.find(separator)
 	{
+		//if it doesn't return none
 		None => None,
-		     Some(index) => 
-		     {
-			     match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) 
-			     {
-				     (Ok(l), Ok(r)) => Some((l,r)),
-				     _ => None
-			     }
-		     }
+		
+		//If it does exist, capture the index from the return of find()
+		Some(index) => 
+		{
+			//This statement splits the string at the seperator and parses both sides from string to T
+			match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) 
+			{
+				//if the above match expression is successful return the option pair
+				(Ok(l), Ok(r)) => Some((l,r)),
+				
+				//if it is anything else return None.	
+				_ => None
+			}
+		}
 	}
 }
 
