77559e9d6143a1a6e63e40be7e837cc131dc512a
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-30 13:08:07 -0400

    wrote a function to parse two parameters with a seperator between them

--- Mini Projects/MandelbrotPlot/src/main.rs
+++ Mini Projects/MandelbrotPlot/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::str:FromStr;
+
+fn parse_pair<T: FromStr>(s: &str, separator: car) -> Option<(T, T)> 
+{
+	match s.find(separator)
+	{
+		None => None,
+		Some(index) => 
+		{
+			match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) 
+			{
+				(Ok(l), Ok(r)) => Some((l,r)),
+				_ => None
+			}
+		}
+	}
 }
+
+
